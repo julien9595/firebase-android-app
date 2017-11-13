ffc246d2efabe3bd4c8b514d65c32f78475bdc5e
erwanlbp <erwan.lbp@gmail.com>
2017-11-13 19:32:29 +0100

    Ajoute l'upload et download de l'image utilisateur

--- app/src/main/java/eisti/firebase/ProfileActivity.java
+++ app/src/main/java/eisti/firebase/ProfileActivity.java
@@ -7,11 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,11 +26,17 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 
-public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int PICK_IMAGE = 42;
     private FirebaseAuth firebaseAuth;
     private DatabaseReference databaseReference;
+    private StorageReference imageReference;
 
     private EditText editTextNameProfile;
     private EditText editTextAddressProfile;
@@ -35,6 +46,8 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     private Button buttonSaveProfile;
     private Button buttonDeleteProfile;
     private Button buttonLogOut;
+    private ImageView image;
+    private Button btnImage;
 
     private FirebaseUser user;
 
@@ -45,6 +58,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        imageReference = FirebaseStorage.getInstance().getReference().child("images").child(FirebaseAuth.getInstance().getUid());
 
         if (firebaseAuth.getCurrentUser() == null) {
             finish();
@@ -60,15 +74,25 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         buttonSaveProfile = findViewById(R.id.buttonSaveProfile);
         buttonDeleteProfile = findViewById(R.id.buttonDeleteProfile);
 
+        image = findViewById(R.id.imageView);
+        btnImage = findViewById(R.id.btnImage);
+
         buttonSaveProfile.setOnClickListener(this);
         buttonDeleteProfile.setOnClickListener(this);
         buttonLogOut.setOnClickListener(this);
+        btnImage.setOnClickListener(this);
 
         editTextNameProfile = findViewById(R.id.editTextNameProfile);
         editTextAddressProfile = findViewById(R.id.editTextAddressProfile);
 
         this.getUserInformation();
-        textViewProfile.setText(getString(R.string.welcome) + this.user.getEmail());
+        textViewProfile.setText(getString(R.string.welcome) + " " + this.user.getEmail());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadImage();
     }
 
     private void updateTextView(DataSnapshot dataSnapshot) {
@@ -76,10 +100,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         switch (key) {
             case "name":
-                textViewNameProfile.setText(getString(R.string.yourName)+" "+dataSnapshot.getValue(String.class));
+                textViewNameProfile.setText(dataSnapshot.getValue(String.class));
                 break;
             case "address":
-                textViewAddressProfile.setText(getString(R.string.yourAddress)+" "+dataSnapshot.getValue(String.class));
+                textViewAddressProfile.setText(dataSnapshot.getValue(String.class));
                 break;
         }
     }
@@ -173,6 +197,59 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         });
     }
 
+    private void loadImage() {
+        // Comment faire de base
+
+        final long ONE_MEGABYTE = 1024 * 1024;
+        imageReference.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                // Data is returns, use this as needed
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
+
+
+        // SPECIAL pour les images
+        Glide.with(this.getApplicationContext())
+                .using(new FirebaseImageLoader())
+                .load(imageReference)
+                .diskCacheStrategy(DiskCacheStrategy.NONE) // Pour éviter le caching
+                .skipMemoryCache(true)                     // Pour éviter le caching
+                .into(image);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE) {
+            try {
+                Toast.makeText(ProfileActivity.this, "Uploading ...", Toast.LENGTH_SHORT).show();
+                UploadTask uploadTask = imageReference.putStream(this.getContentResolver().openInputStream(data.getData()));
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(ProfileActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                        exception.printStackTrace();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                        loadImage();
+                        Toast.makeText(ProfileActivity.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } catch (Exception e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Override
     public void onClick(View v) {
         if (v == buttonLogOut) {
@@ -187,6 +264,13 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         if (v == buttonDeleteProfile) {
             deleteUser();
+        }
+
+        if (v.getId() == R.id.btnImage) {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
         }
     }
 }
